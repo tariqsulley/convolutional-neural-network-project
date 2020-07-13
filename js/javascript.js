fce1025e60c7596c5abc2a3775d5a0e0cda85c00
tariqsulley <32623579+tariqsulley@users.noreply.github.com>
2020-07-13 16:45:24 +0000

    Create javascript.js

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+var button = document.getElementById("Top_Btn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
